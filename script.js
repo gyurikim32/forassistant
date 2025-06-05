1dfe3f93056e6d958fb7b77a4e3f450f5549ebaf
gyurikim32 <48764721+gyurikim32@users.noreply.github.com>
2025-06-05 16:26:56 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function goToNextPage() {
-  const name = document.getElementById("nameInput").value;
-  if (name) {
-    localStorage.setItem("username", name);
-    alert("Welcome, " + name + "! (We’ll make the next page soon!)");
-  } else {
-    alert("Please enter your name!");
-  }
-}
